352ccfe7bc1752d6ea695438e6919cadf57b00f4
Michal Ratajewski <michal.ratajewski@pearson.com>
2020-06-19 12:33:20 +0200

    Adding two views for movie favourite flaging and movie rating.

---
+++ movie-app-server/gui/src/main/java/org/example/controller/MovieFavourite.java
@@ -0,0 +1,15 @@
+package org.example.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+
+import java.awt.*;
+
+public class MovieFavourite {
+    @FXML
+    private TextField tfMovieName;
+    @FXML
+    private CheckBox chbFavourite;
+    @FXML
+    private Button btnConfirm;
+}
---
+++ movie-app-server/gui/src/main/java/org/example/controller/MovieRate.java
@@ -0,0 +1,15 @@
+package org.example.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+import java.awt.*;
+
+public class MovieRate {
+    @FXML
+    private TextField tfMovieRate;
+    @FXML
+    private ChoiceBox choicebRate;
+    @FXML
+    private Button btnDone;
+}
